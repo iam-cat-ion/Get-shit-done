da465add74373a63e4929831294f9dc0a7d9d721
iam-cat-ion <diazyizzy@gmail.com>
2025-07-13 12:57:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -33,27 +33,3 @@ function completeTask(button, xp) {
     colors: ['#000000', '#333333', '#999999', '#CCCCCC', '#FFFFFF']
 });
 }
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open('getshitdone-cache').then(function(cache) {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/quest.html',
-        '/style.css',
-        '/script.js',
-        '/Untitled44_20250713123604.png',
-        '/Untitled45_20250713124143.png'
-      ]);
-    })
-  );
-});
-
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
-  );
-});
-     
